f695063bff7f8fd5d8db06c77921d38a1e960bd8
Pranav-Shepal <143519184+Pranav-Shepal@users.noreply.github.com>
2025-07-21 12:10:02 +0530

    Time: 15 ms (1.65%), Space: 15.8 MB (32.99%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,38 @@
 class Solution {
 public:
+
+    void sort(int idx,int n,vector<int>& nums){
+        while(idx<=n){
+            swap(nums[idx],nums[n]);
+            idx++;
+            n--;
+        }
+    }
+
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        int n=nums.size();
+
+        int idx=-1;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                idx=i;
+                break;
+            }
+        }
+
+        if(idx==-1) {
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+        
+        for(int i=0;i<n;i++)cout<<nums[i]<<" ";
+
+        for(int i=n-1;i>=0;i--){
+            if(nums[i]>nums[idx]){
+                swap(nums[i],nums[idx]);
+                break;
+            }
+        }
+        sort(idx+1,n-1,nums);
     }
 };
