5c7332200591bfc784520c9e14ea34d2aab60de7
Pranav-Shepal <143519184+Pranav-Shepal@users.noreply.github.com>
2025-07-27 15:55:42 +0530

    Time: 0 ms (100%), Space: 8.4 MB (36.7%) - LeetHub

---
+++ 2365-percentage-of-letter-in-string/2365-percentage-of-letter-in-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int percentageLetter(string s, char letter) {
+        int n=s.length();
+    
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(s[i]==letter) cnt++;
+        }   
+        double per=((double)cnt/(double)n)*100;
+
+        return per;
+
+    }
+};
