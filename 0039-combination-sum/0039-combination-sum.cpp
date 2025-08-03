188b1a863a190e0159302c8a919a771aa17f24c4
Pranav-Shepal <143519184+Pranav-Shepal@users.noreply.github.com>
2025-08-03 17:09:20 +0530

    Time: 99 ms (6.55%), Space: 69.6 MB (9.69%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
 
-    void f(vector<vector<int>>& ans,vector<int>&v,int n, int target,vector<int>nums){
+    void f(vector<vector<int>>& ans,vector<int>v,int n, int target,vector<int>nums){
         if(target==0){
             ans.push_back(v);
             return;
         }
-        // if(n==nums.size()-1){
-        //     if(nums[n]==target){
-        //         v.push_back(nums[n]);
-        //         ans.push_back(v);
-        //         v.pop_back();
-        //     }
-        //     return;
-        // }
         if(n>=nums.size())return;
+        // not take
         f(ans,v,n+1,target,nums);
+        // if possible - take
         if(target-nums[n]>=0) {
             v.push_back(nums[n]);
             f(ans,v,n,target-nums[n],nums);
-            v.pop_back();
         }
     }       
 
