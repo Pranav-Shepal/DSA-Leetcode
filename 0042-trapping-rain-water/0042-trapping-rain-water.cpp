ee5544a6cacd61c687d2409fef9353dc18e0be25
Pranav-Shepal <143519184+Pranav-Shepal@users.noreply.github.com>
2025-08-14 21:48:03 +0530

    Time: 0 ms (100%), Space: 27.4 MB (8.4%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,29 +2,33 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n=height.size();
-        vector<int>prevMax(n);
-        vector<int>nextMax(n);
-        int maxNum=height[0];
-        prevMax[0]=-1;
-        for(int i=1;i<n;i++){
-            prevMax[i]=maxNum;
-            if(maxNum<height[i])maxNum=height[i];
+        
+        vector<int>nge(n);
+        vector<int>pge(n);
+        nge[n-1]=height[n-1];
+        pge[0]=height[0];
+
+        int mxEle=height[n-1];
+        for(int i=n-2;i>=0;i--){
+            nge[i]=mxEle;
+            if(mxEle<height[i])mxEle=height[i];
         }
 
-        maxNum=height[n-1];
-        nextMax[n-1]=-1;
-        for(int i=n-2;i>=0;i--){
-            nextMax[i]=maxNum;
-            if(maxNum<height[i])maxNum=height[i];
+        mxEle=height[0];
+        for(int i=1;i<n;i++){
+            pge[i]=mxEle;
+            if(mxEle<height[i])mxEle=height[i];
         }
-        int units=0;
-        for(int i=1;i<=n-2;i++){
-            if(min(nextMax[i],prevMax[i])>height[i]){
-                units+=min(nextMax[i],prevMax[i])-height[i];
+
+        int ans=0;
+
+        for(int i=1;i<n-1;i++){
+            int mn=min(pge[i],nge[i]);
+            if(mn>height[i]){
+                ans+=mn-height[i];
             }
         }
-
-        return units;
-
+       
+        return ans;
     }
 };
