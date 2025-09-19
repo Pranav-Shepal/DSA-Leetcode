2d4b7538af6b303d56d85947f79c4038c4bee085
Pranav-Shepal <143519184+Pranav-Shepal@users.noreply.github.com>
2025-09-19 22:43:19 +0530

    Time: 0 ms (100%), Space: 29.7 MB (38.22%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
 
     void rev(vector<int>& nums, int i,int j){
-        while(i<=j){
-            swap(nums[i],nums[j]);
-            i++;j--;
+        while(i<j){
+            swap(nums[i++],nums[j--]);
         }
-
     }
 
     void rotate(vector<int>& nums, int k) {
         int n=nums.size();
         if(k>n)k=k%n;
+
         rev(nums,0,n-k-1);
         rev(nums,n-k,n-1);
         rev(nums,0,n-1);
+
     }
 };
