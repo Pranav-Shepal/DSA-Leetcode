f0b73432ca9420176d79157d1a8ab74b020e56a4
Pranav-Shepal <143519184+Pranav-Shepal@users.noreply.github.com>
2025-09-20 20:41:54 +0530

    Time: 0 ms (100%), Space: 10.2 MB (35.44%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,33 +1,20 @@
 class Solution {
 public:
-
-    vector<int> rev(vector<int>v){
-        int n=v.size();
-        for(int i=0,j=n-1;i<=j;i++,j--){
-            swap(v[i],v[j]);
-        }
-
-        return v;
-    }
-
     void rotate(vector<vector<int>>& matrix) {
         int n=matrix.size();
-        vector<vector<int>>trr(n,vector<int>(n,0));
 
+        // transpose
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                trr[i][j]=matrix[j][i];
+            for(int j=i;j<n;j++){
+                swap(matrix[i][j],matrix[j][i]);
             }
-        }for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                cout<<trr[i][j]<<" ";
-            }
-            cout<<endl;
-        }
-        for(int i=0;i<n;i++){
-            vector<int> v=trr[i];
-            matrix[i]=rev(v);
         }
 
+    
+        for(int i=0;i<n;i++){
+            auto v=matrix[i];
+            reverse(v.begin(),v.end());
+            matrix[i]=v;
+        }
     }
 };
