f35e6d9b3c8c39f1f2e7a40fc013b9a99052edcc
Pranav-Shepal <143519184+Pranav-Shepal@users.noreply.github.com>
2025-09-22 11:36:11 +0530

    Time: 3 ms (17.9%), Space: 27.3 MB (19.71%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,33 +2,30 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n=height.size();
-        
-        vector<int>nge(n);
-        vector<int>pge(n);
-        nge[n-1]=height[n-1];
+
+        vector<int>pge(n,0);
+        vector<int>nge(n,0);
+
         pge[0]=height[0];
-
-        int mxEle=height[n-1];
-        for(int i=n-2;i>=0;i--){
-            nge[i]=mxEle;
-            if(mxEle<height[i])mxEle=height[i];
-        }
-
-        mxEle=height[0];
+        int mxEle=INT_MIN;
         for(int i=1;i<n;i++){
+            mxEle=max(pge[i-1],height[i]);
             pge[i]=mxEle;
-            if(mxEle<height[i])mxEle=height[i];
         }
 
-        int ans=0;
+        nge[n-1]=height[n-1];
+        mxEle=INT_MIN;
+        for(int i=n-2;i>=0;i--){
+            mxEle=max(nge[i+1],height[i]);
+            nge[i]=mxEle;
+        }
 
+        int units=0;
         for(int i=1;i<n-1;i++){
-            int mn=min(pge[i],nge[i]);
-            if(mn>height[i]){
-                ans+=mn-height[i];
-            }
+            int mnEl=min(nge[i],pge[i]);
+            if(mnEl>height[i])units+=mnEl-height[i];
         }
-       
-        return ans;
+
+        return units;
     }
 };
