0523d565905f21268a528075ccd89c6341ddaa85
Pranav-Shepal <143519184+Pranav-Shepal@users.noreply.github.com>
2026-01-09 20:05:01 +0530

    Time: 0 ms (100%), Space: 8.5 MB (53.68%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long int ans=0;
+        bool neg=false;
+        if(x<0){
+            neg=true;
+        }
+        int rem=0;
+        while(x){
+            int rem=x%10;
+            ans=ans*10+rem;
+            x=x/10;
+        }
+        if(ans > INT_MAX || ans < INT_MIN) return 0;
+        return ans;
+    }
+};
