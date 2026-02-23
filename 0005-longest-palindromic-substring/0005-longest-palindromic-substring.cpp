eafa3a536527147e84a1bcdd26b23f8401db6b83
Pranav-Shepal <143519184+Pranav-Shepal@users.noreply.github.com>
2026-02-23 22:44:13 +0530

    Time: 979 ms (6.47%), Space: 471.3 MB (5%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -3,27 +3,34 @@ public:
     string longestPalindrome(string s) {
         int n=s.length();
 
-        int mxlen=INT_MIN;
+        int l=0;
+        int r=n-1;
+        string fans="";
         string ans="";
-        for(int i=0;i<n;i++){
-            int l=i,r=i;
-            // for odd length
-            while(l>=0 && r<n && s[l]==s[r]){
-                if(r-l+1>mxlen)ans=s.substr(l,r-l+1);
-                mxlen=max(mxlen,r-l+1);
-                l--;r++;
-            }
 
-            // for even length
-            l=i;
-            r=i+1;
-            while(l>=0 && r<n && s[l]==s[r]){
-                if(r-l+1>mxlen)ans=s.substr(l,r-l+1);
-                mxlen=max(mxlen,r-l+1);
-                l--;r++;
+        for(int i=0;i<n;i++){
+            ans="";
+            l=r=i;
+            while(l>=0 && r<=n-1 && s[l]==s[r]){
+                ans=s.substr(l,r-l+1);
+                if(ans.length()>fans.length()){
+                    fans=ans;
+                }
+                l--;
+                r++;
+            }
+            
+            l=i;r=i+1;
+            while(l>=0 && r<=n-1 && s[l]==s[r]){
+                ans=s.substr(l,r-l+1);
+                if(ans.length()>fans.length()){
+                    fans=ans;
+                }
+                l--;
+                r++;
             }
         }
-        cout<<mxlen;
-        return ans;
+
+        return fans;
     }
 };
