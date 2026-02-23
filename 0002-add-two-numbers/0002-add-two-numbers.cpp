53c5641c220faf4c469c78b2e96ff7fab1547a13
Pranav-Shepal <143519184+Pranav-Shepal@users.noreply.github.com>
2026-02-23 23:04:27 +0530

    Time: 36 ms (7.45%), Space: 77.1 MB (44.98%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -1,35 +1,29 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        
+        int sum=0;
+        int carry=0;
         ListNode* dummy=new ListNode(-1);
         ListNode* t=dummy;
 
-        int carry=0;
-
         while(l1 || l2 || carry){
-            int val1=l1 ? l1->val : 0;
-            int val2=l2 ? l2->val : 0;
+            int a=l1 ? l1->val : 0;
+            int b=l2 ? l2->val : 0;
 
-            int sum=val1+val2+carry;
 
-            t->next=new ListNode(sum%10);
-            carry=sum/10;
+            sum+=a+b+carry;
+            ListNode* node=new ListNode(sum%10);
+            t->next=node;
             t=t->next;
+            cout<<t->val<<" ";
+
+            carry=sum/10;
+            sum=0;
 
             if(l1)l1=l1->next;
             if(l2)l2=l2->next;
         }
-
         return dummy->next;
     }
 };
